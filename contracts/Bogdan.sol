6d2befd8bd18b346d6ec058afaf1864ead6b187f
Bogdan <zebogdan@proton.me>
2023-08-22 14:33:24 +0300

    added erc20permit

--- contracts/Bogdan.sol
+++ contracts/Bogdan.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Bogdan is ERC20 {
-    constructor() ERC20("BOGDAN", "$BOGDAN") {
+contract Bogdan is ERC20, ERC20Permit {
+    constructor() ERC20("BOGDAN", "$BOGDAN") ERC20Permit("BOGDAN") {
         _mint(msg.sender, 420690000000000e18);
     }
 }
